9a45f3b9675997fb5daf9629274c8f46e7ab4653
Teja Kumar Muppala <142835834+tejakumarsai6309@users.noreply.github.com>
2023-10-24 17:53:59 -0500

    Update bonus.js

--- routes/bonus.js
+++ routes/bonus.js
@@ -5,14 +5,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    x=req.query.x;
-    if(x==undefined)
-    x=(Math.random()*100);
-    f1=Math.fround(x);
-    f2=Math.random(x);
-    f3=Math.round(x);
+    z=req.query.z;
+    if(z==undefined)
+    z=(Math.random()*100);
+    f1=Math.fround(z);
+    f2=Math.random(z);
+    f3=Math.round(z);
   
-  res.send(`fround of ${x} is ${f1},random of ${x} is ${f2} and round of ${x} is ${f3} `);
+  res.send(`fround of ${z} is ${f1},random of ${z} is ${f2} and round of ${z} is ${f3} `);
   
 });
 
