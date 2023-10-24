df4eb445e927dc0a8f2f484c47239213c3d5dee1
Teja Kumar Muppala <142835834+tejakumarsai6309@users.noreply.github.com>
2023-10-24 17:41:24 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+sum = 0;
+more = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more = more+1;
+  sum+=more;
+  res.send(`sum is ${sum}`);
 });
+
 
 module.exports = router;
